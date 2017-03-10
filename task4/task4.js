1472c6cc25e47ca4933c5cfdd03c05a0f12ea378
Agressif <sylarfarewell@gmail.com>
2017-03-10 17:01:12 +0800

    vue1

--- task4/task4.js
+++ task4/task4.js
@@ -1,23 +1,25 @@
 ﻿//观察者构造函数
-function Observer(data) {
-    this.data = data;
-}
-
-
-	Object.defineProperty(this.data,key,{
+function Observer(data){
+	this.data = data;
+	for (let val in data){
+		Object.defineProperty(this.data,val,{
 		enumberable: true,
 		configurable: true,
 		get:function(){
-			console.log("你访问了" + key);
+			console.log("你访问了" + val);
 			return val;
 		},
 		set:function(newval){
-			console.log("你设置了" + key + ",新的值为" + newval);
+			console.log("你设置了" + val + ",新的值为" + newval);
 			if(newval === val) return;
 			val = newval;
-		}
-	})
+			}
+		});
+	}
+	
+}
 
+	
 let user = new Observer({
     name: "lixiaoyan",
     age: "23",
